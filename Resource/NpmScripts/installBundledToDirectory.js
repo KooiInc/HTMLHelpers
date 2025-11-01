ee2681e67d499e1bf9c143edb4143098207280f0
Renzo Kooi <renzo.kooi@gmail.com>
2025-11-01 12:01:46 +0100

    installBundledTo info in package.json and create folder if not existing.

--- Resource/NpmScripts/installBundledToDirectory.js
+++ Resource/NpmScripts/installBundledToDirectory.js
@@ -5,24 +5,28 @@ if (process.argv.length < 3) {
   process.exit(1);
 }
 
-const directory2InstallTo = process.argv[2];
-
-const directoryExists = fs.existsSync(directory2InstallTo);
-
-if (!directoryExists) {
-  console.log(`It seems that [${directory2InstallTo}] does not exist.` +
-     ` Create it first please` );
-  process.exit(1);
-}
-
+const directory2InstallTo = maybeCreateDirectory(process.argv[2].trim());
 const files = {
   "htmlhelpers.min.js": fs.readFileSync(`./Bundle/htmlhelpers.min.js`),
   "htmlhelpers.script.min.js": fs.readFileSync(`./Bundle/htmlhelpers.script.min.js`),
 };
-
 Object.entries(files).forEach( ([fileName, file]) => {
   fs.writeFileSync(`${directory2InstallTo}/${fileName}`, file);
   console.log(`File ${fileName} written to ${directory2InstallTo}`);
 });
 
 process.exit(0);
+
+function maybeCreateDirectory(directory2InstallTo) {
+  const directoryExists = fs.existsSync(directory2InstallTo);
+  
+  if (!directoryExists) {
+    try {
+      fs.mkdirSync(directory2InstallTo);
+      return directory2InstallTo;
+    } catch(err) {
+      console.log(`Directory [${directory2InstallTo}] can not be created, probably invalid name.`);
+      process.exit(1);
+    }
+  }
+}
